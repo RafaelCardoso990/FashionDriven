1bd14e38bd96f68a14df4973e611f39a38f3476e
Rafael Ferreira <rafaelcardoso990@gmail.com>
2022-02-25 00:26:35 -0300

    Projeto Finalizado

--- script-tela.2.js
+++ script-tela.2.js
@@ -4,39 +4,40 @@ let modeloCamiseta;
 let modeloGola;
 let modeloTecido;
 let objeto = [];
+let pedidoPronto;
 const nome = prompt('Digite o seu nome :')
 
-function escolherModelo(modelo){
+function escolherModelo(modelo) {
     let selecionado = document.querySelector('.primeira-escolha .selecionado');
-    if(selecionado !== null){
+    if (selecionado !== null) {
         selecionado.classList.toggle('selecionado')
     }
     modelo.classList.add('selecionado')
     selecionou = 'sim'
-    
-    modeloCamiseta = document.querySelector('.primeira-escolha .selecionado .tipo').innerHTML;
-    
-    console.log(modeloCamiseta)   
-}   
 
-function escolherGola(gola){
+    modeloCamiseta = document.querySelector('.primeira-escolha .selecionado .tipo').innerHTML;
+
+    console.log(modeloCamiseta)
+}
+
+function escolherGola(gola) {
     let selecionado = document.querySelector('.segunda-escolha .selecionado');
-    if(selecionado !== null){
+    if (selecionado !== null) {
         selecionado.classList.toggle('selecionado')
     }
     gola.classList.add('selecionado')
 
     selecionou = 'sim'
-    
+
     modeloGola = document.querySelector('.segunda-escolha .selecionado .tipo').innerHTML;
-    
+
     console.log(modeloGola)
-  
+
 }
 
-function escolherTecido(tecido){
+function escolherTecido(tecido) {
     let selecionado = document.querySelector('.terceira-escolha .selecionado');
-    if(selecionado !== null){
+    if (selecionado !== null) {
         selecionado.classList.toggle('selecionado')
     }
     tecido.classList.add('selecionado')
@@ -48,84 +49,134 @@ function escolherTecido(tecido){
     console.log(modeloTecido)
 
     ativarbotao();
-   
+
 }
 
-function ativarbotao(){
+function ativarbotao() {
     let botaoAtivado = document.querySelector('.botao');
-    if(selecionou.length >= 3){
+    if (selecionou.length >= 3) {
         botaoAtivado.classList.add('ativo');
     }
 }
 
-function validarInput(){
+function validarInput() {
     link = document.querySelector('.link').value;
-    if(link == ''){
+    if (link == '') {
         alert('Digite um link de referência.')
-    }else{
+    } else {
         alert('Confirmando encomenda')
-        
+
     }
-}  
-
-function armazenarDados(){
-    objeto.push({
-        model: modeloCamiseta,
-        neck: modeloGola,
-        material: modeloTecido,
-        owner: nome,
-        image: link,
-        author: nome
-    })
-
-    
 }
 
-function confirmarPedido(){
+function confirmarPedido() {
     validarInput();
-    armazenarDados();
+    // armazenarDados();
     postarDados();
 }
 
-function postarDados(){
-    const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', objeto);
-    promise.then((resposta)=>{
-        alert('Encomenda confirmada.');
-        console.log(resposta.data);
-        const envioPedido = document.querySelector(`.link`)
-        for(let i = 0; i < envioPedido.length; i++){
-        envioPedido.innerHTML = `<div>   
-                                    <div><img src="${resposta.data[i].image}" alt="camiseta"></div>
-                                    <p><b>Criador</b>: ${resposta.data[i].owner}</p>
-                                </div>`;
-        }                        
+function postarDados() {
+    const request = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', {
+        model: modeloCamiseta,
+        neck: modeloGola,
+        material: modeloTecido,
+        image: link,
+        owner: nome,
+        author: nome
     });
-    promise.catch(postErro);
-} 
+    request.then(postSucesso);
+    request.catch(postErro);
+}
 
+function postSucesso(reposta) {
+    alert('Encomenda feita com sucesso.');  
+    mostrarBlusasCriadas();
+}
 
-// function postSucesso(reposta){
-    
-    
-// }
-
-function postErro(){
+function postErro() {
     alert('Ops, não conseguimos processar sua encomenda.');
 }
 
-function mostrarBlusasCriadas(){
-    const promise = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts') 
-    promise.then(processarResposta);
+function mostrarBlusasCriadas() {
+    const request = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts')
+    request.then(processarResposta);
 }
 
-function processarResposta(resposta){
-    console.log(resposta.data)
+function processarResposta(resposta) {
     const camisasCriadas = document.querySelector('.ultimos-pedidos')
-    for(let i = 0; i <resposta.data.length; i++){
-    camisasCriadas.innerHTML +=` <div>   
-                                    <div><img src="${resposta.data[i].image}" alt="camiseta"></div>
-                                    <p><b>Criador</b>: ${resposta.data[i].owner}</p>
-                                </div>`
+    camisasCriadas.innerHTML = '';
+    console.log(resposta.data)
+    for (let i = 0; i < resposta.data.length; i++) {
+        camisasCriadas.innerHTML += ` <div class="refazer" onclick="pegarBlusaCriada(this)">   
+                                        <div><img src="${resposta.data[i].image}" alt="camiseta"></div>
+                                        <p class="img"><b>Criador</b>: ${resposta.data[i].owner}</p>
+                                      </div>`
     }
+}
+
+function pegarBlusaCriada(camiseta){
+    let selecionado = document.querySelector('.ultimos-pedidos .selecionado-camiseta');
+    
+    if (selecionado !== null) {
+        selecionado.classList.toggle('selecionado-camiseta')
+    }
+    camiseta.classList.add('selecionado-camiseta')
+    
+    pedidoPronto = document.querySelector('.ultimos-pedidos .selecionado-camiseta .img').innerHTML;
+    console.log(pedidoPronto)
+
+   confirmandoEncomenda(); 
+}
+
+function confirmandoEncomenda(){
+    let confirma = confirm('Você quer criar uma blusa igual a essa ?')
+    
+    if(confirma === true){
+        pegandoPedido();
+    }   
+}
+
+function pegandoPedido(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts')
+    promise.then(comparandoPedidos);
+    
+}   
+
+function comparandoPedidos(resposta1){
+     
+    for(i = 0; i < resposta1.data.length; i++){
+        if(pedidoPronto.includes(resposta1.data[i].owner) === true){
+            // console.log(resposta1.data[i].image)
+        
+            modeloCamiseta = resposta1.data[i].model,
+            modeloGola = resposta1.data[i].neck,
+            modeloTecido = resposta1.data[i].material,
+            link = resposta1.data[i].image
+    }
+    }
+    
+    enviarEncomendaPronta();
+}
+
+function enviarEncomendaPronta(){
+    const request = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', {
+        model: modeloCamiseta,
+        neck: modeloGola,
+        material: modeloTecido,
+        image: link,
+        owner: nome,
+        author: nome
+    });
+    request.then(enviouComSucesso)
+    request.catch(naoEnviouComSucesso)
+}
+
+function enviouComSucesso(){
+    alert('Enviou com Sucesso.')
+    mostrarBlusasCriadas();
+}
+
+function naoEnviouComSucesso(){
+    alert('Não enviou com Sucesso.')
 }
 mostrarBlusasCriadas();
